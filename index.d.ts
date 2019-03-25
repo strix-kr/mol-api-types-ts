6c0e3a6eba16cc981e8a3fffbff53699a4788295
dehypnosis <dehypnosis@gmail.com>
2019-03-25 14:31:53 +0900

    update types

--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,7 @@ import { GraphQLResolveInfo } from "graphql";
 declare namespace APIGateway {
 
   namespace UnderlyingService {
+
     // Service schema of underlying service.
     interface ServiceSchema extends Moleculer.ServiceSchema {
       // Metadata of underlying service.
@@ -13,14 +14,28 @@ declare namespace APIGateway {
 
         // Internal usage to compare api configuration between same named services.
         apiVersion?: string
-      } & Moleculer.GenericObject;
+      } & Moleculer.GenericObject
+
+      actions:  { [key: string]: Action | ActionHandler }
     }
 
-    // Service actions will be published by following the given configurations.
+    // Service Action interface of underlying service.
+    interface Action extends Moleculer.Action {
+      handler?: ActionHandler;
+    }
+
+    type ActionHandler<T = any> = ((ctx: Moleculer.Context<Moleculer.GenericObject, ActionContextMeta>) => PromiseLike<T> | T) & ThisType<Moleculer.Service>;
+
+    interface ActionContextMeta extends Moleculer.GenericObject {
+      graphql?: { source: any, args: any, context: GraphQLRequestContext, info: GraphQLResolveInfo }
+      user?: any
+      locale?: string
+    }
+
+    // Service actions can be published with api configurations in metadata.
     interface APIConfig {
       rest?: RESTConfig
       graphql?: GraphQLConfig
-      guard?: GuardConfig
     }
 
     // Configuration to map service actions to REST endpoints.
@@ -74,7 +89,7 @@ declare namespace APIGateway {
     // Configuration to extends GraphQL schema and map service actions to GraphQL schema resolver.
     interface GraphQLConfig {
       /*
-        schema: Either a string or strings which defines the GraphQL schema for the service.
+        typeDefs: Either a string or strings which defines the GraphQL schema for the service.
 
         `
         extend Query {
@@ -92,7 +107,7 @@ declare namespace APIGateway {
         }
         `
       */
-      schema: string
+      typeDefs: string | string[]
 
       /*
         resolvers: Defines the ways to resolve the given schema.
@@ -114,50 +129,73 @@ declare namespace APIGateway {
       action: string
 
       /*
-        batch: If true, API Gateway will use the batch loader (GraphQL DataLoader) to resolve the given field.
-        In this case, the mapped action should be able to serve the request with batching for first param.
+        params: Params key/value map to call the given action.
+
+        1) Map a param from field params.
+        The params option { id: "@.id" } will make resolver to call the action "post.get" with { userId: 'id value of the field params' } params.
+        "Query.user": {
+          action: "iam.user.get",
+          params: {
+            id: "@.id"
+          }
+        }
+
+        2) Map a param from the root object props.
+        The params option { userId: "$.id" } will make resolver to call the action "post.get" with { userId: 'id value of the root object props' } params.
+        "User.post": {
+          action: "post.get",
+          params: {
+            userId: "$.id"
+          }
+        }
+
+        3) Define a param value directly.
+        Otherwise, given primitive value will be used directly to call the action.
+        "Query.user": {
+          action: "iam.user.get",
+          params: {
+            ignoreDisabledUser: true
+          }
+        }
+       */
+      params?: { [paramName: string]: any }
+
+      /*
+        paramsWithBatch: If any param names given, API Gateway will use the batch loader (GraphQL DataLoader) to resolve the given field.
+        In this case, the mapped action should be able to serve the request with batched params.
+
+        "Query.user": {
+          action: "iam.user.get",
+          params: {
+            id: "@.id"
+          }
+          paramsWithBatch: ["id"]
+        }
+
+        Then "iam.user.get" action should be able to serve the request with { id: ["user-1", "user-2", ...] } params.
 
         "User.post": {
           action: "post.get",
-          batch: true
+          params: {
+            userId: "$.id"
+          },
+          paramsWithBatch: ["userId"]
         }
 
-        In above example, "post.get({ id, ... })" action should be able to serve { id: ["post-1", "post-2", ...] } request.
+        In above example, "post.get" action should be able to serve { userId: ["user-1", "user-2", ...] } params.
        */
-      batch?: boolean
+      paramsWithBatch?: string[]
 
       /*
         ignoreError: If true, "null" will be returned on error.
        */
       ignoreError?: boolean
     }
+  }
 
-
-    // Guard configuration to protect action call via Either GraphQL or REST endpoint.
-    interface GuardConfig {
-      // TODO: 1 GUARD with IAM.USER/ADMIN Context
-    }
-
-    // Action Metadata of underlying service.
-    interface ActionMeta extends Moleculer.GenericObject {
-      api?: ActionAPIMeta
-    }
-
-    // Underlying services can use below meta data to deal with requests from API Gateway.
-    interface ActionAPIMeta extends APIRequestContext {
-      graphql: { source: any, args: any, context: GraphQLRequestContext, info: GraphQLResolveInfo }
-    }
-
-    interface APIRequestContext {
-      // TODO: guard integration...
-      user: any
-      locale: string
-    }
-
-    // Internal usage
-    interface GraphQLRequestContext extends APIRequestContext {
-      moleculer?: Moleculer.Context
-    }
+  // Internal usage
+  interface GraphQLRequestContext {
+    moleculer?: Moleculer.Context
   }
 
   // internal usage
